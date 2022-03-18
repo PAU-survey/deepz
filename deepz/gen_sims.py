a250d84dd4bd81fcde06b6b6cbf41b2702602990
Martin B. Eriksen <eriksen@pic.es>
2022-03-18 21:16:06 +0100

    Script for generating simulations.

---
+++ deepz/gen_sims.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+#from IPython.core import debugger as ipdb
+import os
+import dask
+import dask.config
+import dask.dataframe as dd
+from dask.distributed import Client, LocalCluster
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from time import sleep
+from dask.diagnostics import ProgressBar
+from astropy.cosmology import Planck15 
+import fsps
+
+bands = fsps.filters.list_filters()
+
+#client = Client('tcp://193.109.175.131:35060')
+
+def empty_gen(params):
+    """Generate an empty set of data."""
+   
+    os.environ['SPS_HOME'] = '/nfs/astro/eriksen/source/fsps'
+ 
+    # This hard coding should actually not be needed..
+    L = ['zred', 'logzsol', 'tau', 'const', 'sf_start', 'dust2']
+    mags = np.zeros((len(params), len(bands)))
+    
+    import fsps
+    pop = fsps.StellarPopulation(zcontinuous=1, add_neb_emission=True, sfh=1, dust_type=2)
+    for i, (_, row) in enumerate(params.iterrows()):    
+        # Note, here we have modified fsps to not panick..
+        for key in L:
+            pop.params[key] = row[key]
+        
+        mags[i] = pop.get_mags(tage=row.tage)
+    
+    return pd.DataFrame(mags, columns=bands, index=params.index)
+
+
+# Parameters defining the ranges. Based on numbers from Cigal.
+ranges = {\
+'zred': [0, 1.5],
+'logzsol': [-0.5, 0.2],
+#'tage': [1, 13.7],
+'gas_logu': [-4., 1.],
+'tau': [0.1, 12],
+'const': [0, 0.25],
+'dust2': [0, 0.6]}
+
+
+def gen_params(Ngal):
+    """Generate the distribution of parameters."""
+
+    print('starting...')
+    # Input parameters.
+    D = {}
+    for key, (low,high) in ranges.items():
+        D[key] = np.random.uniform(low, high, size=Ngal)
+
+    # Following:
+    # https://github.com/dfm/python-fsps/issues/68
+    # I have connected the age and redshift using the cosmology.
+    D['tage'] = np.array(Planck15.age(D['zred']))
+    D['sf_start'] = np.random.random(Ngal)*D['tage'] #-0.1)
+ 
+    # For consistency, the gas phase metallicity should be set to
+    # the same value as for the galaxy (FSPS documentation).
+    D['gas_logz'] = D['logzsol'] 
+    print('finished random numbers...')
+
+    df1 = pd.DataFrame(D)
+
+    return df1
+
+def make_sim(df1, out_path):
+    """Create the simulations."""
+
+    df2 = dd.from_pandas(df1, chunksize=10000)
+    #df2 = dd.from_pandas(df1, chunksize=3000)
+    df2.to_parquet(str(out_path / 'params.parquet'))
+
+    meta = [(x, float) for x in bands]
+    G = df2.map_partitions(empty_gen, meta=meta)
+
+    print('Staring parallel..')
+#    with dask.config.set(scheduler='processes'), ProgressBar():
+    with ProgressBar():
+    #with dask.config.set(scheduler='single-threaded'):
+        G.to_parquet(str(out_path / 'mags.parquet'))
+
+
+Ngal = int(1e6)
+#Ngal = int(1e2)
+Ngal = int(1e6)
+out_path = Path('/nfs/astro/eriksen/deepz/sims/v9')
+
+if __name__ == '__main__':
+    df1 = gen_params(Ngal)
+
+#    cluster = LocalCluster(n_workers=8, threads_per_worker=1)
+    client = Client('tcp://193.109.175.131:44657')
+    make_sim(df1, out_path)
