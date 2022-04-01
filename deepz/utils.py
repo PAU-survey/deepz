7877c44bdc3788fa160f9a848be64aa58817e05d
Martin B. Eriksen <eriksen@pic.es>
2022-04-02 00:38:30 +0200

    Comments and remove all function.

--- deepz/utils.py
+++ deepz/utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # encoding: UTF8
 
+# Various utils.
+
 import sys
 import numpy as np
 import torch
@@ -11,7 +13,11 @@ import networks
 
 
 def get_nets(path_base, use_mdn, pretrain=True, Nbands=46):
-    """Initialize networks."""
+    """Initialize networks.
+       :param path_base: {str} Path to the pretrained networks.
+       :param use_mdn: {bool} If using a mixture density network.
+       :param Nbands: {int} Number of bands.
+    """
     
     Nfeat = 10
     Nl = 5
@@ -38,7 +44,11 @@ def get_nets(path_base, use_mdn, pretrain=True, Nbands=46):
 
 
 def get_loaders(ifold, inds, data):
-    """Get loaders for specific fold."""
+    """Get loaders for specific fold.
+       :param ifold: {int} The ifold to use.
+       :param inds: {array} Indices to use.
+       :param data: {tuple} All the data to use.
+    """
     
     flux, flux_err, fmes, vinv, isnan, zbin, ref_id = data
     
@@ -54,19 +64,7 @@ def get_loaders(ifold, inds, data):
     ds_train = sub(ix_train)
     ds_test = sub(ix_test)
 
-
     train_dl = DataLoader(ds_train, batch_size=500, shuffle=True)
     test_dl = DataLoader(ds_test, batch_size=100)
     
     return train_dl, test_dl, zbin[ix_test]
-
-
-def all_inds():
-    """Load a matrix with all the indices."""
-
-#    index_path = '/nfs/pic.es/user/e/eriksen/papers/deepz/redux/inds_large_v1.txt'
-    index_path = '/nfs/astro/eriksen/deepz/inds/inds_large_v1.txt'
-    inds_all = np.loadtxt(index_path)
-    inds_all = torch.from_numpy(inds_all)
-    
-    return inds_all
