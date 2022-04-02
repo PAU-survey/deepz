7cedab4fc12a1c383998a68825cf3f7bb3254017
Martin B. Eriksen <eriksen@pic.es>
2022-04-02 19:37:27 +0200

    Adopt pretrain to new format

--- deepz/pretrain_sim.py
+++ deepz/pretrain_sim.py
@@ -4,16 +4,16 @@
 # Pretrain the network on simulations. Later the trained network
 # would be the start for training on data.
 
-import sys
 import os
-
+import sys
 import time
+
 from pathlib import Path
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
-sys.path.append('var')
 from itertools import chain
+from IPython.core import debugger
 
 import torch
 from torch import nn
@@ -21,16 +21,12 @@ from torch import nn, optim
 from torch.utils.data.dataset import random_split
 from torch.utils.data import TensorDataset, DataLoader
 
+import networks
 
-path_in = Path('/data/astro/eriksen/deepz/sims/v9/')
+path_in = Path('/data/astro/scratch/eriksen/deepz/sims/v9/')
 
 mags_df = pd.read_parquet(str(path_in / 'mags.parquet'))
 params_df = pd.read_parquet(str(path_in / 'params.parquet'))
-
-
-
-config = {'use_enc': False}
-
 
 # Bands to select for the training...
 NB = ['nb{}'.format(x) for x in 455+10*np.arange(40)]
@@ -71,32 +67,31 @@ test_dl = DataLoader(test_ds, batch_size=100)
 
 
 
-import networks
-
 # Different networks.
-Nfeat = 10
-Nl = 5
-enc = networks.Encoder(Nfeat=Nfeat, Nl=Nl).cuda()
-dec = networks.Decoder(Nfeat=Nfeat, Nl=Nl).cuda()
+#Nfeat = 10
+#Nl = 5
+#enc = networks.Encoder(Nfeat=Nfeat, Nl=Nl).cuda()
+#dec = networks.Decoder(Nfeat=Nfeat, Nl=Nl).cuda()
+#net_pz = networks.MDNNetwork(len(bands)+Nfeat).cuda()
 
-net_pz = networks.MDNNetwork(len(bands)+Nfeat).cuda()
+Nbands = len(bands)
 
-loss_function = nn.CrossEntropyLoss()
 
-def xloss_function(net, pred, target):        
-    loss = loss_function(pred, Bzbin.cuda())
-    
-    return loss
+net = networks.Deepz(Nbands).cuda()
 
-params_chain = chain(enc.parameters(), dec.parameters(), net_pz.parameters())
-optimizer = optim.Adam(params_chain, lr=1e-3)
+#params_chain = chain(enc.parameters(), dec.parameters(), net_pz.parameters())
+#params_chain = chain(enc.parameters(), dec.parameters(), net_pz.parameters())
+#optimizer = optim.Adam(params_chain, lr=1e-3)
+optimizer = optim.Adam(net.parameters(), lr=1e-3)
 
 for i in range(10):
     L = []
-    enc.train()
-    dec.train()
-    net_pz.train()
-    
+#    enc.train()
+#    dec.train()
+#    net_pz.train()
+   
+    net.train()
+ 
     t1 = time.time()
     for Bparams, Bzbin, Bflux, Bflux_err in train_dl:
         t3 = time.time()
@@ -107,22 +102,26 @@ for i in range(10):
         
         noise = Bflux_err * torch.randn(Bflux.shape).cuda()
         Bflux = Bflux + noise
-        feat = enc(Bflux)
-        if not config['use_enc']:
-            feat = 0.*feat
+#        feat = enc(Bflux)
 
-        Xinp = torch.cat([Bflux, feat], 1) # In train
+#        Xinp = torch.cat([Bflux, feat], 1) # In train
         
         Bzbin = Bzbin.cuda()
         
         Bz = 0.001*Bzbin.type(torch.float)
-        _, loss_pz = net_pz.loss(Xinp, Bz)  
+#        _, loss_pz = net_pz.loss(Xinp, Bz)  
         
         # And then
-        loss_recon = (dec(feat) - Bflux).abs() / Bflux_err
+#        loss_recon = (dec(feat) - Bflux).abs() / Bflux_err
+#        loss_recon = loss_recon.mean()
+#        loss = loss_pz + loss_recon
+
+        pred, recon, loss_pz = net.pred_recon_loss(Bflux, Bflux, Bz)
+        loss_recon = (recon - Bflux).abs() / Bflux_err
         loss_recon = loss_recon.mean()
-        
+
         loss = loss_pz + loss_recon
+
         loss.backward()
         optimizer.step()    
         L.append(loss.item())
@@ -133,24 +132,32 @@ for i in range(10):
     loss_train = sum(L) / len(L)
     L = []
     dxL = []
-    enc.eval()
-    dec.eval()
-    net_pz.eval()
+#    enc.eval()
+#    dec.eval()
+#    net_pz.eval()
+
+    net.eval()
 
     print('starting test eval')
     for Bparams, Bzbin, Bflux, Bflux_err in test_dl:
         Bflux = Bflux.cuda()
-        feat = enc(Bflux)
-        if not config['use_enc']:
-            feat = 0.*feat
+        Bflux_err = Bflux_err.cuda()
+#        feat = enc(Bflux)
 
-        Xinp = torch.cat([Bflux, feat], 1) # In test.
-        pred = net_pz(Xinp.cuda())
+#        Xinp = torch.cat([Bflux, feat], 1) # In test.
+#        pred = net_pz(Xinp.cuda())
         
         # For some reason we did *not* include the reconstruction loss here.
         Bzbin = Bzbin.cuda()
         Bz = 0.001*Bzbin.type(torch.float)
-        _, loss = net_pz.loss(Xinp, Bz)    
+#        _, loss = net_pz.loss(Xinp, Bz)    
+
+        pred, recon, loss_pz = net.pred_recon_loss(Bflux, Bflux, Bz)
+
+        loss_recon = (recon - Bflux).abs() / Bflux_err
+        loss_recon = loss_recon.mean()
+
+        loss = loss_pz + loss_recon
         
         L.append(loss.item())
         
@@ -170,14 +177,20 @@ for i in range(10):
     print(i, loss_train, loss_test, 'sig68', sig68, 'outl', outl)
 
 
-version = 9
-sim = 'fsps'
-output_dir = Path('/data/astro/scratch/eriksen/deepz/redux/pretrain')
+version = 1
+output_dir = Path('/data/astro/scratch/eriksen/deepz/clear')
+path = output_dir / f'pretrain_{version}.pt'
+
+if path.exists():
+    print('Path already exists...')
+    sys.exit(1)
+
 if not os.path.exists(output_dir):
     os.mkdir(output_dir)
 
-    
-path_base = str(output_dir / (f'v{version}'+'_{}.pt'))
-torch.save(enc.state_dict(), path_base.format('enc'))
-torch.save(dec.state_dict(), path_base.format('dec'))
-torch.save(net_pz.state_dict(), path_base.format('pz'))
+torch.save(net.state_dict(), path)
+
+#path_base = str(output_dir / (f'v{version}'+'_{}.pt'))
+#torch.save(enc.state_dict(), path_base.format('enc'))
+#torch.save(dec.state_dict(), path_base.format('dec'))
+#torch.save(net_pz.state_dict(), path_base.format('pz'))
