37090b5406ecc8bbe1e1de637e46120e1eaa4f4f
Martin B. Eriksen <eriksen@pic.es>
2022-04-03 17:26:57 +0200

    Add the lisence text to the source code

--- deepz/trainer.py
+++ deepz/trainer.py
@@ -1,6 +1,23 @@
 #!/usr/bin/env python
 # encoding: UTF8
 
+# Copyright (C) 2022 Martin B. Eriksen
+# This file is part of Deepz <https://github.com/PAU-survey/deepz>.
+#
+# Deepz is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Deepz is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Deepz.  If not, see <http://www.gnu.org/licenses/>.
+
+
 from IPython.core import debugger as ipdb
 import time
 import numpy as np
